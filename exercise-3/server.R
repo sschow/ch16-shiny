51676b4740a1f35faeef135cfc10e908fde122c4
Samantha C <sschow@uw.edu>
2018-05-09 16:45:38 -0700

    exercise 3 done in class

--- exercise-3/server.R
+++ exercise-3/server.R
@@ -3,6 +3,24 @@
 library(shiny)
 library(ggplot2)
 # Create a shiny server that creates a scatterplot. 
+server <- function(input, output){
+  output$scatter <- renderPlot({
+    x_data <- mpg[[input$`x-variable`]]
+    y_data <- mpg[[input$`y-variable`]]
+    
+  
+    ggplot(mpg) +
+      geom_point(mapping = aes(x = x_data, y = y_data),
+                 size=input$size,
+                 color=input$color) +
+      ggtitle(paste0(x_data, " vs. ", y_data)) +
+      xlab(input$`x-variable`) +
+      ylab(input$`y-variable`)
+    
+  })
+}
+
+
 
 # It should take as an input the x_var, y_var, color, and size
 # Save the result of `renderPlot` to output$scatter
@@ -14,4 +32,5 @@ library(ggplot2)
 
     
     # Create ggplot scatter
+shinyServer(server)
 
--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -1,10 +1,26 @@
 # UI for scatterplot
 library(shiny)
+library(ggplot2)
 
 # Get a vector of column names (from `mpg`) to use as select inputs
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
+ui<- fluidPage(
+  h1("mpg dataset visualization"),
+  selectInput("x-variable", label = "x-variable",
+              choices = select_values, selected = 1),
+  selectInput("y-variable", label = "y-variable",
+              choices = select_values, selected = 1),
+  sliderInput("size", label = "size of point", min = 1, 
+              max = 10, value = 5),
+  selectInput("color", label = "color",
+              choices = c("purple", "blue", "red", "orange", "green"), 
+              selected = 1),
+  
+  plotOutput("scatter")
+  
+  )
 
   # A page header
 
@@ -23,3 +39,4 @@ select_values <- colnames(mpg)
 
 
   # Plot the output with the name "scatter"
+shinyUI(ui)
